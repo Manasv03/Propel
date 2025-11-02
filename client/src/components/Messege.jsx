92e23c93c63cb35b35b88f73bdcf21ee0b45c8be
Manas Verma <manas03verma@gmail.com>
2025-11-02 15:25:24 +0530

    getting started with poject basic setup

---
+++ client/src/components/Messege.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Messege = () => {
+  return (
+    <div>
+      Message
+    </div>
+  )
+}
+
+export default Messege
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyUserData } from "../assets/assets";
+
+
+const AppContext = createContext()
+
+export const AppContextProvider = ({children}) => {
+    
+    const navigate = useNavigate()
+    const [user,setUser] = useState(null);
+    const [chats, setChats] = useState([]);
+    const [selectedChat, setSelectedChat] = useState(null);
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+    const fetchUser = async () => {
+        setUser(dummyUserData); // Replace with actual user fetching logic
+    }
+    
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+    const value = {
+        navigate
+    }
+    
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = ()=> useContext(AppContext)
