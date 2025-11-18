bdd99ebba3072c1423f6107b76202f892c07fb1a
Manas Verma <manas03verma@gmail.com>
2025-11-18 12:42:58 +0530

    almost completed frontend and starting with backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,12 @@ import Community from './pages/Community'
 import { assets } from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
+import Login from './pages/Login'
+import { useAppContext } from './context/AppContext'
 
 const App = () => {
+
+  const {user} = useAppContext();
 
   const [isMenuOpen,setIsMenuOpen]= useState(false);
 
@@ -19,6 +23,8 @@ const App = () => {
   return (
     <>
     {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=>setIsMenuOpen(true)}/>}
+
+    {user ? (
       <div className='dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white'>
         <div className='flex h-screen w-screen'>
           <SideBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
@@ -29,6 +35,11 @@ const App = () => {
           </Routes>
         </div>
       </div>
+    ): (
+      <div className='bg-linear-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen'>
+        <Login/>
+      </div>
+    )}  
     </>
   )
 }
